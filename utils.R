a72f1bd72c15a25023744f0086378d6921af9ee6
Simon Topp <stopp@usgs.gov>
2021-06-14 15:43:28 -0400

    Started putting together workshop modelling pipeline.

---
+++ utils.R
@@ -0,0 +1,36 @@
+fui.hue <- function(R, G, B) {
+  
+  # Convert R,G, and B spectral reflectance to dominant wavelength based
+  # on CIE chromaticity color space
+  
+  # see Wang et al 2015. MODIS-Based Radiometric Color Extraction and
+  # Classification of Inland Water With the Forel-Ule
+  # Scale: A Case Study of Lake Taihu
+  
+  require(colorscience)
+  # chromaticity.diagram.color.fill()
+  Xi <- 2.7689*R + 1.7517*G + 1.1302*B
+  Yi <- 1.0000*R + 4.5907*G + 0.0601*B
+  Zi <- 0.0565*G + 5.5943*B
+  
+  # calculate coordinates on chromaticity diagram
+  x <-  Xi / (Xi + Yi +  Zi)
+  y <-  Yi / (Xi + Yi +  Zi)
+  z <-  Zi / (Xi + Yi +  Zi)
+  
+  # calculate hue angle
+  alpha <- atan2( (x - 0.33), (y - 0.33)) * 180/pi
+  
+  # make look up table for hue angle to wavelength conversion
+  cie <- cccie31 %>%
+    mutate(a = atan2( (x - 0.33), (y - 0.33)) * 180/pi) %>%
+    dplyr::filter(wlnm <= 700) %>%
+    dplyr::filter(wlnm >=380)
+  
+  # find nearest dominant wavelength to hue angle
+  wl <- cie[as.vector(sapply(alpha,function(x) which.min(abs(x - cie$a)))), 'wlnm']
+  
+  #out <- cbind(as.data.frame(alpha), as.data.frame(wl))
+  
+  return(wl)
+}
